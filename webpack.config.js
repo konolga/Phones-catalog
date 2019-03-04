b72d014b2f66a497495dfd5b445898caff27f68e
olgush <psh@ukr.net>
2019-03-04 17:53:20 +0200

    babel-loader with helper

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,24 @@ module.exports={
         path: path.resolve(__dirname, 'src'),//__dirname-->from global
         filename: 'bundle.js'
     },
-    watch: true, // after the initial build continue to watch for changes 
+    watch: true, // after the initial build continue to watch for changes, otherwise use npx webpack or ./node_modules/.bin/webpack
     devtool: "source-map",//to see original source while debugging
     watchOptions: {
         aggregateTimeout: 600
+      },
+      module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-transform-runtime']
+              }
+            }
+          }
+        ]
       }
 }
